a90ef8a68bb2a6f5ec413af71c429f46187e68d7
Labordoronvier <102153713+Labordoronvier@users.noreply.github.com>
2026-09-13 20:00:19 +0800

    Delete src directory

--- src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Card({ children, className = "", style, ...rest }) {
-  return (
-    <div className={["card", className].filter(Boolean).join(" ")} style={style} {...rest}>
-      {children}
-    </div>
-  );
-}
